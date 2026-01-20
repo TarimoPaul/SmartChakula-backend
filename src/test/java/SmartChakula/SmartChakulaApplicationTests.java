bedcd8731aa72f5925c3227660f7fda32d0b5e42
TarimoPaul <chiefmaster7066@gmail>
2026-01-20 13:07:55 +0300

    Initial commit

---
+++ src/test/java/SmartChakula/SmartChakulaApplicationTests.java
@@ -0,0 +1,13 @@
+package SmartChakula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartChakulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
