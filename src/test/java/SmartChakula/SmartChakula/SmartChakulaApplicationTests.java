bd3b8a91091cd748d19d457a13e3114adb0ff8e6
TarimoPaul <chiefmaster7066@gmail>
2026-01-20 12:20:16 +0300

    Initial commit - smart setup

---
+++ src/test/java/SmartChakula/SmartChakula/SmartChakulaApplicationTests.java
@@ -0,0 +1,13 @@
+package SmartChakula.SmartChakula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartChakulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
